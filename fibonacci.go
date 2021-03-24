eb26036e9446198a1c271798e93a7676df4dc83a
evgeniy <pyankov.ev@gmail.com>
2021-03-24 22:21:47 +0300

    fibonacci

---
+++ fibonacci.go
@@ -0,0 +1,25 @@
+// fibonacci closure
+
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	f0 := 0
+	f1 := 1
+
+	return func() int {
+		fib := f0
+		f0, f1 = f1, f0+f1
+		return fib
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}

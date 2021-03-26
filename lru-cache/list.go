070e98c7731a332983ef87b541e54476114028cf
evgeniy <pyankov.ev@gmail.com>
2021-03-27 01:59:43 +0300

    Add PushBack and Remove methods to list

--- lru-cache/list.go
+++ lru-cache/list.go
@@ -7,9 +7,9 @@ type List interface {
 	First() *ListElement
 	Last() *ListElement
 	PushFront(v interface{}) *ListElement
+	PushBack(v interface{}) *ListElement
 	Values() []interface{}
-	// PushBack(v interface{}) *ListElement
-	// Remove(i *ListElement)
+	Remove(el *ListElement)
 	// MoveToFront(i *ListElement)
 }
 
@@ -56,6 +56,21 @@ func (l *list) PushFront(v interface{}) *ListElement {
 	return el
 }
 
+func (l *list) PushBack(v interface{}) *ListElement {
+	el := &ListElement{Value: v, Prev: l.last}
+
+	if l.size == 0 {
+		l.first = el
+		l.last = el
+	} else {
+		l.last.Next = el
+		l.last = el
+	}
+	l.size++
+
+	return el
+}
+
 func (l list) Values() []interface{} {
 	values := make([]interface{}, l.size, l.size)
 	for i, el := 0, l.first; el != nil; i, el = i+1, el.Next {
@@ -63,6 +78,20 @@ func (l list) Values() []interface{} {
 	}
 
 	return values
+}
+
+func (l *list) Remove(el *ListElement) {
+	if el == l.last {
+		l.last = el.Prev
+		el.Prev.Next = nil
+	} else if el == l.first {
+		l.first = el.Next
+		el.Next.Prev = nil
+	} else {
+		el.Prev.Next, el.Next.Prev = el.Next, el.Prev
+	}
+
+	l.size--
 }
 
 func main() {
@@ -76,14 +105,26 @@ func main() {
 	}{
 		{name: "el1", Value: "foo"},
 		{name: "el2", Value: "boo"},
+		{name: "el3", Value: "bar"},
+		{name: "el4", Value: "bar4"},
 	}
 
-	for _, el := range elements {
+	for _, el := range elements[:2] {
 		newEl := list.PushFront(el)
 		fmt.Printf("new pushed el: %v\n", newEl.Value)
 	}
 
+	penultEl := list.PushBack(elements[len(elements)-2])
+	fmt.Printf("new pushed last el: %v\n", penultEl.Value)
+	lastEl := list.PushBack(elements[len(elements)-1])
+	fmt.Printf("new pushed last el: %v\n", lastEl)
 	fmt.Printf("list: %v\n", list)
+
+	list.Remove(penultEl)
+	fmt.Printf("list: %v\n", list)
+	fmt.Println(list.Len())
+	fmt.Printf("last el: %v\n", lastEl.Prev != penultEl)
+	list.Remove(lastEl)
 	fmt.Println(list.Len())
 
 	fmt.Printf("list values: %v\n", list.Values())

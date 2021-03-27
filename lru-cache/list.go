5abf105ad59349295cc19dbdac7d8d98ac5ea117
evgeniy <pyankov.ev@gmail.com>
2021-03-27 03:03:47 +0300

    Implement MoveToFront method

--- lru-cache/list.go
+++ lru-cache/list.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package lrucache
 
 type List interface {
 	Len() int
@@ -10,7 +8,7 @@ type List interface {
 	PushBack(v interface{}) *ListElement
 	Values() []interface{}
 	Remove(el *ListElement)
-	// MoveToFront(i *ListElement)
+	MoveToFront(i *ListElement)
 }
 
 type list struct {
@@ -71,6 +69,21 @@ func (l *list) PushBack(v interface{}) *ListElement {
 	return el
 }
 
+func (l *list) MoveToFront(el *ListElement) {
+	if el == l.first {
+		return
+	} else if el == l.last {
+		l.last = el.Prev
+		el.Prev.Next = nil
+	} else {
+		el.Prev.Next, el.Next.Prev = el.Next, el.Prev
+	}
+
+	l.first.Prev = el
+	l.first = el
+	el.Prev = nil
+}
+
 func (l list) Values() []interface{} {
 	values := make([]interface{}, l.size, l.size)
 	for i, el := 0, l.first; el != nil; i, el = i+1, el.Next {
@@ -94,38 +107,38 @@ func (l *list) Remove(el *ListElement) {
 	l.size--
 }
 
-func main() {
-	list := NewList()
-	fmt.Println(list)
-	fmt.Println(list.Len())
+// func main() {
+// 	list := NewList()
+// 	fmt.Println(list)
+// 	fmt.Println(list.Len())
 
-	elements := []struct {
-		name  string
-		Value string
-	}{
-		{name: "el1", Value: "foo"},
-		{name: "el2", Value: "boo"},
-		{name: "el3", Value: "bar"},
-		{name: "el4", Value: "bar4"},
-	}
+// 	elements := []struct {
+// 		name  string
+// 		Value string
+// 	}{
+// 		{name: "el1", Value: "foo"},
+// 		{name: "el2", Value: "boo"},
+// 		{name: "el3", Value: "bar"},
+// 		{name: "el4", Value: "bar4"},
+// 	}
 
-	for _, el := range elements[:2] {
-		newEl := list.PushFront(el)
-		fmt.Printf("new pushed el: %v\n", newEl.Value)
-	}
+// 	for _, el := range elements[:2] {
+// 		newEl := list.PushFront(el)
+// 		fmt.Printf("new pushed el: %v\n", newEl.Value)
+// 	}
 
-	penultEl := list.PushBack(elements[len(elements)-2])
-	fmt.Printf("new pushed last el: %v\n", penultEl.Value)
-	lastEl := list.PushBack(elements[len(elements)-1])
-	fmt.Printf("new pushed last el: %v\n", lastEl)
-	fmt.Printf("list: %v\n", list)
+// 	penultEl := list.PushBack(elements[len(elements)-2])
+// 	fmt.Printf("new pushed last el: %v\n", penultEl.Value)
+// 	lastEl := list.PushBack(elements[len(elements)-1])
+// 	fmt.Printf("new pushed last el: %v\n", lastEl)
+// 	fmt.Printf("list: %v\n", list)
 
-	list.Remove(penultEl)
-	fmt.Printf("list: %v\n", list)
-	fmt.Println(list.Len())
-	fmt.Printf("last el: %v\n", lastEl.Prev != penultEl)
-	list.Remove(lastEl)
-	fmt.Println(list.Len())
+// 	list.Remove(penultEl)
+// 	fmt.Printf("list: %v\n", list)
+// 	fmt.Println(list.Len())
+// 	fmt.Printf("last el: %v\n", lastEl.Prev != penultEl)
+// 	list.Remove(lastEl)
+// 	fmt.Println(list.Len())
 
-	fmt.Printf("list values: %v\n", list.Values())
-}
+// 	fmt.Printf("list values: %v\n", list.Values())
+// }

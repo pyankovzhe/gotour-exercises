e37922830572e2b33401d01100bac90d8a102fb0
evgeniy <pyankov.ev@gmail.com>
2021-03-27 03:06:09 +0300

    Implement basic lru cache

---
+++ lru-cache/cache.go
@@ -0,0 +1,56 @@
+package lrucache
+
+type Key string
+
+type Cache interface {
+	Set(key Key, value interface{}) bool
+	Get(key Key) (interface{}, bool)
+	Clear()
+}
+
+type lruCache struct {
+	capacity int
+	queue    List
+	items    map[Key]*ListElement
+}
+
+func NewCache(capacity int) Cache {
+	return &lruCache{
+		capacity: capacity,
+		queue:    NewList(),
+		items:    make(map[Key]*ListElement, capacity),
+	}
+}
+
+// The return value is a boolean flag whether the item was present in the cache.
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	if el, ok := c.items[key]; ok {
+		el.Value = value
+		c.queue.MoveToFront(el)
+
+		return true
+	} else {
+		if c.queue.Len() == c.capacity {
+			c.queue.Remove(c.queue.Last())
+			delete(c.items, key)
+		}
+
+		newEl := c.queue.PushFront(value)
+		c.items[key] = newEl
+
+		return false
+	}
+}
+
+func (c lruCache) Get(key Key) (interface{}, bool) {
+	if el, ok := c.items[key]; ok {
+		c.queue.MoveToFront(el)
+		return el.Value, ok
+	}
+
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	// TODO: implement with O(1) complexity
+}

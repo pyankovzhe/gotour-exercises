b082f4850be2ca3cfc5639161f643158b79e1280
evgeniy <pyankov.ev@gmail.com>
2021-03-30 23:51:02 +0300

    Walk btree

--- tree/walk/main.go
+++ tree/walk/main.go
@@ -1,23 +1,44 @@
+// Exercise: Equivalent Binary Trees
+// https://tour.golang.org/concurrency/7
+
 package main
 
 import (
+	"fmt"
+
 	"github.com/pyankovzhe/go-tour/tree/btree"
 )
 
-func Walk(t *btree.Tree, ch chan int) {
+func WalkTree(t *btree.Tree, ch chan int) {
 	if t == nil {
 		close(ch)
 		return
 	}
+
+	walk(t.Root, ch)
+	close(ch)
+}
+
+func walk(node *btree.Node, ch chan int) {
+	if node == nil {
+		return
+	}
+	ch <- node.Value
+
+	walk(node.Left, ch)
+	walk(node.Right, ch)
 }
 
 func main() {
 	t := btree.NewTree()
 	s := []int{10, -1, -10, 5, 6, 8, 4 - 2}
-	for v := range s {
+	for _, v := range s {
 		t.Insert(v)
 	}
 
-	ch := make(chan int)
-	Walk(t, ch)
+	ch := make(chan int, len(s))
+	go WalkTree(t, ch)
+	for v := range ch {
+		fmt.Println(v)
+	}
 }
